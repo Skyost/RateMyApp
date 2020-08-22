6091d4ea8d92ab8a2313fb83a1333efe1e4c0dbb
Skyost <me@skyost.eu>
2020-08-22 12:28:28 +0200

    Added some extra debugging info.

--- android/src/main/kotlin/fr/skyost/rate_my_app/RateMyAppPlugin.kt
+++ android/src/main/kotlin/fr/skyost/rate_my_app/RateMyAppPlugin.kt
@@ -19,103 +19,108 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-public class RateMyAppPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "rate_my_app")
-      channel.setMethodCallHandler(RateMyAppPlugin())
-    }
-  }
-
-  private var activity: Activity? = null
-  private lateinit var channel : MethodChannel
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "rate_my_app")
-    channel.setMethodCallHandler(this);
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if(activity == null) {
-      result.error("activity_is_null", "Activity is null.", null);
-      return;
-    }
-
-    val activity: Activity = this.activity!!
-    when(call.method) {
-      "launchNativeReviewDialog" -> {
-        val manager: ReviewManager = ReviewManagerFactory.create(activity)
-        val request: Task<ReviewInfo> = manager.requestReviewFlow()
-        request.addOnCompleteListener { task ->
-          if (task.isSuccessful) {
-            val reviewInfo: ReviewInfo = task.result
-            val flow: Task<Void> = manager.launchReviewFlow(activity, reviewInfo)
-            flow.addOnCompleteListener { result.success(true) }
-          } else {
-            result.success(false)
-          }
+public class RateMyAppPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "rate_my_app")
+            channel.setMethodCallHandler(RateMyAppPlugin())
         }
-      }
-      "isNativeDialogSupported" -> result.success(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && isPlayStoreInstalled(activity))
-      "launchStore" -> {
-        goToPlayStore(activity, if (call.hasArgument("appId")) call.argument<String>("appId")!! else activity.applicationContext.packageName)
-        result.success(true)
-      }
-      else -> result.notImplemented()
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private var activity: Activity? = null
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity()
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding)
-  }
-
-  override fun onDetachedFromActivity() {
-    activity = null
-  }
-
-  /**
-   * Returns whether the Play Store is installed on the current device.
-   *
-   * @param activity The activity.
-   *
-   * @return Whether the Play Store is installed on the current device.
-   */
-
-  private fun isPlayStoreInstalled(activity: Activity): Boolean {
-    return try {
-      activity.packageManager.getPackageInfo("com.android.vending", 0);
-      true
-    } catch (ex: PackageManager.NameNotFoundException) {
-      false
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.flutterEngine.dartExecutor, "rate_my_app")
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  /**
-   * Launches a Play Store instance.
-   *
-   * @param activity The activity.
-   * @param applicationId The application ID.
-   */
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (activity == null) {
+            result.error("activity_is_null", "Activity is null.", null);
+            return;
+        }
 
-  private fun goToPlayStore(activity: Activity, applicationId: String) {
-    try {
-      activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$applicationId")));
+        val activity: Activity = this.activity!!
+        when (call.method) {
+          "launchNativeReviewDialog" -> {
+            val manager: ReviewManager = ReviewManagerFactory.create(activity)
+            val request: Task<ReviewInfo> = manager.requestReviewFlow()
+            request.addOnCompleteListener { reviewTask ->
+              when {
+                reviewTask.isSuccessful -> {
+                  val reviewInfo: ReviewInfo = reviewTask.result
+                  val flow: Task<Void> = manager.launchReviewFlow(activity, reviewInfo)
+                  flow.addOnCompleteListener { task -> result.success(task.exception != null) }
+                }
+                reviewTask.exception != null -> {
+                  result.error(reviewTask.exception!!.javaClass.name, reviewTask.exception!!.localizedMessage, null)
+                }
+                else -> {
+                  result.success(false)
+                }
+              }
+            }
+          }
+          "isNativeDialogSupported" -> result.success(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && isPlayStoreInstalled(activity))
+          "launchStore" -> {
+            goToPlayStore(activity, if (call.hasArgument("appId")) call.argument<String>("appId")!! else activity.applicationContext.packageName)
+            result.success(true)
+          }
+            else -> result.notImplemented()
+        }
     }
-    catch(ex: android.content.ActivityNotFoundException) {
-      activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$applicationId")));
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
     }
-  }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    /**
+     * Returns whether the Play Store is installed on the current device.
+     *
+     * @param activity The activity.
+     *
+     * @return Whether the Play Store is installed on the current device.
+     */
+
+    private fun isPlayStoreInstalled(activity: Activity): Boolean {
+        return try {
+            activity.packageManager.getPackageInfo("com.android.vending", 0);
+            true
+        } catch (ex: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
+
+    /**
+     * Launches a Play Store instance.
+     *
+     * @param activity The activity.
+     * @param applicationId The application ID.
+     */
+
+    private fun goToPlayStore(activity: Activity, applicationId: String) {
+        try {
+            activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$applicationId")));
+        } catch (ex: android.content.ActivityNotFoundException) {
+            activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$applicationId")));
+        }
+    }
 }
